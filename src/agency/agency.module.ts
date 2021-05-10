7558fb082d45f983b1201b7729b93075de65d637
Anton Ku <anton.ku.nc@gmail.com>
2021-05-10 16:18:14 +0300

    Add agency module

---
+++ src/agency/agency.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { agencyProviders } from './agency.providers';
+import { AgencyService } from './agency.service';
+import { AgencyController } from './agency.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  providers: [
+    ...agencyProviders,
+    AgencyService,
+  ],
+  controllers: [
+    AgencyController
+  ]
+})
+export class AgencyModule {}
