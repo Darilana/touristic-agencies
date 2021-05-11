68bf854f62d160bf91b142b63e7ff2e56c30749e
Anton Ku <anton.ku.nc@gmail.com>
2021-05-11 18:08:19 +0300

    Fix e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,15 @@ import { Module } from '@nestjs/common';
 import { AgencyModule } from './agency/agency.module';
 import { OfficeModule } from './office/office.module';
 import { TourModule } from './tour/tour.module';
-import Next from 'next';
 import { RenderModule } from 'nest-next';
 import { ViewModule } from './view/view.module';
 
 @Module({
   imports: [
-    RenderModule.forRootAsync(Next({
-      dev: process.env.NODE_ENV !== 'production',
-    })),
     AgencyModule,
     OfficeModule,
     TourModule,
+    RenderModule,
     ViewModule
   ],
 })
