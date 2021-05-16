e97fb47fa1452a4d525008c1bde93116918909b5
Darilana Kudinova <darilana.kudinova@gmail.com>
2021-05-16 13:18:12 +0300

    Added views for agencies and offices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,81 @@
-import { FC } from 'react';
 import type { AppProps /*, AppContext */ } from 'next/app';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import MenuIcon from '@material-ui/icons/Menu';
+import {
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from '@material-ui/core';
+import ContactsIcon from '@material-ui/icons/Contacts';
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  iconButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  content: {
+    minHeight: 'calc(100vh - 120px)',
+  },
+  footer: {
+    borderTop: '1px solid #E7E7E7',
+    textAlign: 'center',
+    padding: '20px',
+    height: '100px',
+    width: '100%',
+    flexShrink: 0,
+  },
+}));
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <div>
-        <h1>Header</h1>
+    <div className={classes.root}>
+      <div className={classes.content}>
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                edge="start"
+                className={classes.iconButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Travel admin
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+        <div>
+          <Component {...pageProps} />
+        </div>
       </div>
-      <div>
-        <Component {...pageProps} />
+      <div className={classes.footer}>
+        <Box display="flex" flexDirection="column">
+          <Box display="flex" alignItems="center" justifyContent="center">
+            <ContactsIcon className={classes.iconButton} />
+            <Typography variant="h6">Контакти:</Typography>
+          </Box>
+          <Typography>travel_admin@gmail.com</Typography>
+          <Typography>+38(063)111-11-11</Typography>
+          <Typography>Україна, м.Харків, проспект Науки 2</Typography>
+        </Box>
       </div>
     </div>
   );
--- pages/views/agency/index.tsx
+++ pages/views/agency/index.tsx
@@ -2,24 +2,93 @@ import * as React from 'react';
 import { Agency } from '../../../src/agency/agency.entity';
 import axios from 'axios';
 import { NextPage } from 'next';
+import {
+  makeStyles,
+  TableContainer,
+  Link,
+  Table,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+  Typography,
+  Box, Accordion, AccordionSummary, AccordionDetails
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import OfficeForm from "../../components/OfficeForm";
+import AgencyDetailsForm from "../../components/AgencyDetailsForm";
 
 interface Props {
-  agencies: Agency[]
+  agencies: Agency[];
 }
 
+const useStyles = makeStyles({
+  tableContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  table: {
+    width: 800,
+  },
+});
+
 const AgencyList: NextPage<Props> = ({ agencies }) => {
-  console.log({ agencies })
+  const classes = useStyles();
+
   return (
     <div>
-      <h1>AgencyList</h1>
-      <pre>{JSON.stringify(agencies, null, 2)}</pre>
+      <Box display="flex" justifyContent="center" mb={4} mt={4}>
+        <Typography variant="h4" color="textSecondary">
+          Список агенцій
+        </Typography>
+      </Box>
+      <Box display="flex" justifyContent="center" mt={4}>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Додати агенцію</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <AgencyDetailsForm />
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+      <TableContainer className={classes.tableContainer}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Назва</TableCell>
+              <TableCell>Номер телефону</TableCell>
+              <TableCell align="right" />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {agencies.map((agency) => (
+              <TableRow key={agency.name}>
+                <TableCell component="th" scope="row">
+                  {agency.name}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {agency.phoneNumber}
+                </TableCell>
+                <TableCell>
+                  <Link href={`agency/${agency.id}`}>Детальніше</Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
 
 export async function getServerSideProps() {
   const props: Props = {
-    agencies: (await axios.get('http://localhost:3000/api/agency')).data
+    agencies: (await axios.get('http://localhost:3000/api/agency')).data,
   };
 
   return { props };
